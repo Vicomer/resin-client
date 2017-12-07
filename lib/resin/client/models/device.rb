8a27df4ed74e8450e37f28d75d415accc4af6e70
MiluchOK <milyukov.alexey@gmail.com>
2017-12-06 22:32:35 -0800

    bumping up a version

--- lib/resin/client/models/device.rb
+++ lib/resin/client/models/device.rb
@@ -2,12 +2,13 @@ module Resin
   module Models
     class Device
 
-      attr_reader :name, :id, :device_type
+      attr_reader :name, :id, :device_type, :uuid
 
       def initialize(json)
         @name = json['app_name']
         @id = json['id']
         @device_type = json['device_type']
+        @uuid = json['uuid']
       end
     end
   end
--- lib/resin/client/version.rb
+++ lib/resin/client/version.rb
@@ -1,3 +1,3 @@
 module Resin
-  VERSION = "0.1.3"
+  VERSION = "0.1.4"
 end
