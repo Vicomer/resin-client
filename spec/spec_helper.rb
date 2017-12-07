d8b02533e96a2d15092f34f53a4615929c6f7b69
MiluchOK <milyukov.alexey@gmail.com>
2017-12-06 21:36:09 -0800

    removing the config file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,10 +5,9 @@ require 'yaml'
 
 RSpec.configure do |config|
   # some (optional) config here
-  conf = YAML.load_file(File.join(File.expand_path(File.dirname(__FILE__)), 'resources/config.yml'))
 
 
   config.before :each do
-    @token = conf["resin_token"]
+    @token = "foo"#conf["resin_token"]
   end
 end
